89dd8f5d4cbd8c1ec77a0af0948419e50cca33c3
lorossi <mail@lorenzoros.si>
2021-03-01 15:13:34 +0100

    moved private functions in new file (utils.go)

---
+++ utils.go
@@ -0,0 +1,96 @@
+package colorize
+
+import "fmt"
+
+// collection of functions used in colorize
+
+// get constant value by masking
+func getValue(c Style) (s string) {
+	// bit masking to get value
+	if c&decorationMask != 0 {
+		return fmt.Sprint(c - decorationMask)
+	} else if c&brightnessMask != 0 {
+		return fmt.Sprint(c - brightnessMask)
+	}
+
+	return fmt.Sprint(c)
+}
+
+// get suffix to end style string
+func getSuffix(c Style) (s string) {
+	if c&brightnessMask != 0 {
+		return brightSuffix
+	}
+	return suffix
+}
+
+// create string from constant
+func createStyleString(c Style) (s string) {
+	s = ""
+	s += prefix
+	s += getValue(c)
+	s += getSuffix(c)
+
+	return s
+}
+
+// create string from rgb
+func createRGBString(r, g, b uint8) (s string) {
+	s = fmt.Sprint(16 + r/51*36 + g/51*6 + b/51)
+	return
+}
+
+// create string from truecolor
+func createTruecolorString(r, g, b uint8) (s string) {
+	s = fmt.Sprintf("%d;%d;%d", r, g, b)
+	return
+}
+
+// finally apply the style
+func applyStyle(style string) (e error) {
+	_, e = fmt.Print(style)
+	return e
+}
+
+// create string to move cursor to xy
+func createCursorXYString(x, y uint8) (s string) {
+	s = fmt.Sprintf("%d;%d;H", y, x)
+	return
+}
+
+// create styled text. This function both takes the style and the string.
+func styledText(color Style, text ...interface{}) (formatted string) {
+	formatted = createStyleString(color)
+	// iterate throught each interface item
+	for _, t := range text {
+		// try to convert interface to string
+		s := fmt.Sprint(t)
+		// if so, remove starting and leading square brackets
+		if len(s) > 1 {
+			formatted += s[1 : len(s)-1]
+		}
+	}
+	// add reset character
+	formatted += createStyleString(Reset)
+	return
+}
+
+// get rgb value from hue
+func hueTorgb(v1, v2, v3 float64) (v float64) {
+	if v3 < 0 {
+		v3++
+	} else if v3 > 1 {
+		v3--
+	}
+
+	if 6*v3 < 1 {
+		return v1 + (v2-v1)*6*v3
+	}
+	if 2*v3 < 1 {
+		return v2
+	}
+	if 3*v3 < 2 {
+		return v1 + (v2-v1)*(2/3-v3)*6
+	}
+	return v1
+}
