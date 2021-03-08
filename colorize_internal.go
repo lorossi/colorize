bff7d1274ce862785406350ac717117ac47c1b6e
lorossi <contact@lorenzoros.si>
2021-03-08 13:18:25 +0100

    Update colorize_internal.go

--- colorize_internal.go
+++ colorize_internal.go
@@ -83,27 +83,6 @@ func styledText(color Style, text ...interface{}) (formatted string) {
 	return
 }
 
-// converts the hue to rgb
-func hueToRgb(v1, v2, h float64) (v float64) {
-	if h < 0 {
-		h++
-	} else if h > 1 {
-		h--
-	}
-
-	if 6*h < 1 {
-		v = v1 + (v2-v1)*6*h
-	} else if 2*h < 1 {
-		v = v2
-	} else if 3*h < 2 {
-		v = v1 + (v2-v1)*(2/3-h)*6
-	} else {
-		v = v1
-	}
-
-	return
-}
-
 // converts HSL (range 0-255) to RGB (range 0-255)
 func hslTOrgb(h, s, l uint8) (r, g, b uint8) {
 	var R, G, B float64
@@ -124,6 +103,27 @@ func hslTOrgb(h, s, l uint8) (r, g, b uint8) {
 			c2 = (L + S) - (L * S)
 		}
 		c1 = 2*L - c2
+
+		// converts the hue to rgb
+		hueToRgb := func(v1, v2, h float64) (v float64) {
+			if h < 0 {
+				h++
+			} else if h > 1 {
+				h--
+			}
+
+			if 6*h < 1 {
+				v = v1 + (v2-v1)*6*h
+			} else if 2*h < 1 {
+				v = v2
+			} else if 3*h < 2 {
+				v = v1 + (v2-v1)*(2/3-h)*6
+			} else {
+				v = v1
+			}
+
+			return
+		}
 
 		R = hueToRgb(c1, c2, H+(1/3))
 		G = hueToRgb(c1, c2, H)
